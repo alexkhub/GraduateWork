d007a72eb842018cc0857853768d08de93b36d46
Aleksandr Khubaev <aleksandrkhubaev04@gmail.com>
2024-01-09 20:32:06 +0300

    create models

--- graduatework_drf/graduatework_drf/settings.py
+++ graduatework_drf/graduatework_drf/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-z!np)3ed=cf*t0jnn_t2&sh_h+d$44hq9)g9q((&)9f901$-yy
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -55,7 +53,6 @@ MIDDLEWARE = [
 
 ]
 
-
 # подключение cors
 CORS_ORIGIN_ALLOW_ALL = True
 #
@@ -73,7 +70,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated'
     ],
 }
-
 
 ROOT_URLCONF = 'graduatework_drf.urls'
 
@@ -95,7 +91,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'graduatework_drf.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -105,7 +100,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -125,7 +119,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -137,7 +130,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -147,3 +139,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Расширение таблицы User
+
+AUTH_USER_MODEL = 'student_performance.Users'
