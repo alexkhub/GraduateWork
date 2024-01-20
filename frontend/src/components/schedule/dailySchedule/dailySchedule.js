aecebde8562554c93a7d52f3532c365b5d02a858
UFxx <smileprost@gmail.com>
2024-01-20 21:10:30 +0300

    Schedule

--- frontend/src/components/schedule/dailySchedule/dailySchedule.js
+++ frontend/src/components/schedule/dailySchedule/dailySchedule.js
@@ -1,61 +1,48 @@
-import Pair from "../daily-schedule/pair/pair";
-import DoublePair from "./doublePair/doublePair";
+import Pair from "../pair/pair";
+import DoublePair from "../doublePair/doublePair";
 
 
-function DailySchedule() {
+function DailySchedule(props) {
     return (
-        <div className="schedule-content">
-            <p>Расписание на сегодня</p>
-            <table className="daily-schedule">
-                <tbody>
+        <>
+            <tr>
+                <td></td>
+                <td className="day">{props.day}</td>
+                <td></td>
+            </tr>
 
-                    <tr>
-                        <td></td>
-                        <td className="day">Понедельник</td>
-                        <td></td>
-                    </tr>
+            <Pair
+                pairNumber='1'
+                subjectName={props.firstSubjectName}
+                teacherName={props.firstTeacherName}
+                audience={props.firstAudience}
+                time="9:00 - 10:30"
+            />
 
-                    <Pair
-                        pairNumber='1'
-                        subjectName='Иностранный язык в профессиональной деятельности'
-                        teacherName='Бигаева Э.С.'
-                        audience='36'
-                        time='9:00 - 13:30'
-                    />
+            <Pair
+                pairNumber='2'
+                subjectName={props.secondSubjectName}
+                teacherName={props.secondTeacherName}
+                audience={props.secondAudience}
+                time="10:40 - 12:10"
+            />
 
-                    <DoublePair
-                          pairNumber='2'
+            <Pair
+                pairNumber='3'
+                subjectName={props.thirstSubjectName}
+                teacherName={props.thirstTeacherName}
+                audience={props.thirstAudience}
+                time="12:50 - 14:20"
+            />
 
-                          subjectName='Технология разработки программного обеспечения'
-                          teacherName='Зембатова М.А.'
-                          audience='27'
-                          
-                          secondSubjectName = 'Разработка мобильных приложений'
-                          secondTeacherName = 'Зангиева С.В.'
-                          secondAudience = '23'
-
-                          time='10:40 - 12:50'
-                    />
-
-                    <Pair
-                        pairNumber='3'
-                        subjectName='Разработка и защита баз данных'
-                        teacherName='Гагиева В.Л.'
-                        audience='64'
-                        time='12:50: - 14:20'
-                    />
-
-                    <Pair
-                        pairNumber='4'
-                        subjectName='Питон'
-                        teacherName='Караев Ч.А.'
-                        audience='70'
-                        time='14:30: - 16:00'
-                    />
-                    
-                </tbody>
-            </table>
-        </div>
+            <Pair
+                pairNumber='4'
+                subjectName={props.fourthSubjectName}
+                teacherName={props.fourthTeacherName}
+                audience={props.fourthAudience}
+                time="14:30 - 16:00"
+            />
+        </>
     )
 }
 
