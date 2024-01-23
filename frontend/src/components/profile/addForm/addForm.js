846becf382f987b2119e28d1f06d1d395f05030f
UFxx <smileprost@gmail.com>
2024-01-23 09:42:29 +0300

    Added add form (profile)

---
+++ frontend/src/components/profile/addForm/addForm.js
@@ -0,0 +1,26 @@
+import './add-form.css';
+
+function AddFrom() {
+    return (
+        <>
+            <form className='add-form'>
+                <div>
+                    <textarea placeholder="Описание задания" />
+                    <input type='file' id='add-form-file-input'/>
+                    <label for='add-form-file-input' >Выберите файл</label>
+                </div>
+                <div>
+                    <select>
+                        <option>-Группа-</option>
+                        <option value="3-1">3-1 ИС</option>
+                        <option value="4-2">4-2 ИС</option>
+                        <option value="1">1 ИС</option>
+                    </select>
+                    <button id='send-button'>Отправить</button>
+                </div>
+            </form>
+        </>
+    )
+}
+
+export default AddFrom;
--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -1,12 +1,14 @@
 import './profile.css';
 import ProfileTopContent from './top-content/top-content';
 import FailedTasks from './failed-tasks/failed-tasks';
+import AddFrom from './addForm/addForm';
 
 function Profile() {
     return (
         <div className='profile-content'>
             <ProfileTopContent />
-            <FailedTasks />
+            {/* <FailedTasks /> */}
+            <AddFrom />
         </div>
     )
 }
