bf3d4d7e152d3a5622939cf4b69e19bae893bbf4
Aleksandr Khubaev <aleksandrkhubaev04@gmail.com>
2024-02-17 12:49:13 +0300

    pog

--- graduatework_drf/timetable/urls.py
+++ graduatework_drf/timetable/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', TimetableListView.as_view(), name='timetable'),
+    path('timetable/<slug:group_slug>', TimetableListView.as_view(), name='timetable'),
     path('exam/<slug:group_slug>/', ExamListView.as_view(), name='exam'),
     path('timetable_changes/', TimetableChangesListView.as_view(), name='timetable_changes'),
     path('timetable_lector/', LectorTimeTableListView.as_view(), name='timetable_lector'),
