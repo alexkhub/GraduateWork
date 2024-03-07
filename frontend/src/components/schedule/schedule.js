9f8c1788ba4779a5fe8089bb2da3a3a4f5f461d2
UFxx <smileprost@gmail.com>
2024-03-07 17:12:57 +0300

    Weekly schedule example

--- frontend/src/components/schedule/schedule.js
+++ frontend/src/components/schedule/schedule.js
@@ -1,4 +1,5 @@
 import DailySchedule from './dailySchedule/dailySchedule';
+import WeeklySchedule from './weeklySchedule/weeklySchedule';
 import './schedule.css';
 
 function Schedule() {
@@ -11,30 +12,6 @@ function Schedule() {
 
                         <DailySchedule
                             day='Понедельник'
-
-                            firstPairNumber='1'
-                            firstSubjectName='Иностранный язык в профессиональной деятельности'
-                            firstTeacherName='Бигаева Э.С.'
-                            firstAudience='36'
-                            firstTime='9:00 - 10:30'
-
-                            secondPairNumber='2'
-                            secondSubjectName='Технология разработки программного обеспечения'
-                            secondTeacherName='Зембатова М.А.'
-                            secondAudience='27'
-                            secondTime='10:40 - 12:10'
-
-                            thirstPairNumber='3'
-                            thirstSubjectName='Разработка мобильных приложений'
-                            thirstTeacherName='Зангиева С.В.'
-                            thirstAudience='23'
-                            thirstTime='12:50 - 14:20'
-
-                            fourthPairNumber='4'
-                            fourthSubjectName='Питон'
-                            fourthTeacherName='Караев Ч.А.'
-                            fourthAudience='70'
-                            fourthTime='14:20 - 16:00'
                         />
 
                     </tbody>
@@ -43,7 +20,14 @@ function Schedule() {
 
             <div className="weekly-schedule schedule-content">
                 <p>Расписание на неделю</p>
-                
+
+                <table className="daily-schedule">
+                    <tbody>
+                        <WeeklySchedule
+                        />
+                    </tbody>
+                </table>
+
             </div>
         </>
     )
