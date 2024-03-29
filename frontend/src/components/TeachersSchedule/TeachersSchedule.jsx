69f60adbeb36609a2c66e67258c5ad33219e21d8
UFxx <smileprost@gmail.com>
2024-03-29 20:10:23 +0300

    Fix pair number, added _fonts.scss

--- frontend/src/components/TeachersSchedule/TeachersSchedule.jsx
+++ frontend/src/components/TeachersSchedule/TeachersSchedule.jsx
@@ -1,90 +1,103 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import DailySchedule from './TeachersDailySchedule/TeachersDailySchedule';
-import DoublePair from './TeachersDoublePair/TeachersDoublePair';
-import Teachers from './TeachersPair/TeachersPair';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DailySchedule from "./TeachersDailySchedule/TeachersDailySchedule";
+import DoublePair from "./TeachersDoublePair/TeachersDoublePair";
+import Teachers from "./TeachersPair/TeachersPair";
 
 function TeachersSchedule() {
-    const [pairs, setPairs] = useState('');
-    const mondayPairs = [];
-    const tuesdayPairs = [];
-    const wednesdayPairs = [];
-    const thursdayPairs = [];
-    const fridayPairs = [];
+  const [pairs, setPairs] = useState("");
+  const mondayPairs = [];
+  const tuesdayPairs = [];
+  const wednesdayPairs = [];
+  const thursdayPairs = [];
+  const fridayPairs = [];
 
-    useEffect(() => {
-        axios.get(`http://127.0.0.1:8000/api-timetable/timetable_lector/`)
-            .then(data => setPairs(data.data.lector_timetable))
-        }, []);
+  useEffect(() => {
+    axios
+      .get(`http://127.0.0.1:8000/api-timetable/timetable_lector/`)
+      .then((data) => setPairs(data.data.lector_timetable));
+  }, []);
 
-    console.log(pairs)
+  console.log(pairs);
 
-    for (let i = 0; i < pairs.length; i++) {
-        pairs[i].lecturer.user = pairs[i].lecturer.user.replace('-', ' ').replace('_', ' ')
+  for (let i = 0; i < pairs.length; i++) {
+    pairs[i].lecturer.user = pairs[i].lecturer.user
+      .replace("-", " ")
+      .replace("_", " ");
 
-        let pair = <Teachers pairNumber={pairs[i].lesson_number} subjectName={pairs[i].subject} groupName={pairs[i].group} audience={pairs[i].classroom} time={`${pairs[i].start_time} - ${pairs[i].end_time}`} key={pairs[i].id} />;
+    let pair = (
+      <Teachers
+        pairNumber={pairs[i].lesson_number}
+        subjectName={pairs[i].subject}
+        groupName={pairs[i].group}
+        audience={pairs[i].classroom}
+        time={`${pairs[i].start_time} - ${pairs[i].end_time}`}
+        key={pairs[i].id}
+      />
+    );
 
-        if (pairs[i].evenness === 'совмещенная') {
-            pair = <DoublePair
-                pairNumber={pairs[i].lesson_number}
-                subjectName={pairs[i].subject}
-                groupName={pairs[i].group}
-                audience={pairs[i].classroom}
-                time={`${pairs[i].start_time} - ${pairs[i].end_time}`}
-                secondSubjectName=''
-                secondGroupName=''
-                secondAudience=''
-                key={pairs[i].id}
-            />;
-        }
-
-        switch (pairs[i].day_of_the_week) {
-            case 'понедельник' || 'Понедельник':
-                mondayPairs.push(pair)
-                break;
-            case 'вторник' || 'Вторник':
-                tuesdayPairs.push(pair)
-                break
-            case 'среда' || 'Среда':
-                wednesdayPairs.push(pair)
-                break;
-            case 'четверг' || 'Четверг':
-                thursdayPairs.push(pair)
-                break;
-            case 'пятница' || 'Пятница':
-                fridayPairs.push(pair)
-                break;
-            default:
-                return
-        }
+    if (pairs[i].evenness === "совмещенная") {
+      pair = (
+        <DoublePair
+          pairNumber={pairs[i].lesson_number}
+          subjectName={pairs[i].subject}
+          groupName={pairs[i].group}
+          audience={pairs[i].classroom}
+          time={`${pairs[i].start_time} - ${pairs[i].end_time}`}
+          secondSubjectName=""
+          secondGroupName=""
+          secondAudience=""
+          key={pairs[i].id}
+        />
+      );
     }
 
-    return (
-        <>
-            <div className="teachers-daily-schedule teachers-schedule-content">
-                <p>Расписание на завтра</p>
-                <table className="daily-schedule">
-                    <tbody>
-                        <DailySchedule day='Понедельник' pairs={mondayPairs} />
-                    </tbody>
-                </table>
-            </div>
+    switch (pairs[i].day_of_the_week) {
+      case "понедельник" || "Понедельник":
+        mondayPairs.push(pair);
+        break;
+      case "вторник" || "Вторник":
+        tuesdayPairs.push(pair);
+        break;
+      case "среда" || "Среда":
+        wednesdayPairs.push(pair);
+        break;
+      case "четверг" || "Четверг":
+        thursdayPairs.push(pair);
+        break;
+      case "пятница" || "Пятница":
+        fridayPairs.push(pair);
+        break;
+      default:
+        return;
+    }
+  }
 
-            <div className="weekly-schedule schedule-content">
-                <p>Расписание на неделю</p>
-                <table className="daily-schedule">
-                    <tbody>
-                        <DailySchedule day='Понедельник' pairs={mondayPairs} />
-                        <DailySchedule day='Вторник' pairs={tuesdayPairs} />
-                        <DailySchedule day='Среда' pairs={wednesdayPairs} />
-                        <DailySchedule day='Четверг' pairs={thursdayPairs} />
-                        <DailySchedule day='Пятница' pairs={fridayPairs} />
-                    </tbody>
-                </table>
+  return (
+    <>
+      <div className="daily-schedule schedule-content">
+        <p>Расписание на завтра</p>
+        <table className="daily-schedule">
+          <tbody>
+            <DailySchedule day="Понедельник" pairs={mondayPairs} />
+          </tbody>
+        </table>
+      </div>
 
-            </div>
-        </>
-    )
+      <div className="schedule-content">
+        <p className="schedule-content-title">Расписание на неделю</p>
+        <table className="daily-schedule">
+          <tbody>
+            <DailySchedule day="Понедельник" pairs={mondayPairs} />
+            <DailySchedule day="Вторник" pairs={tuesdayPairs} />
+            <DailySchedule day="Среда" pairs={wednesdayPairs} />
+            <DailySchedule day="Четверг" pairs={thursdayPairs} />
+            <DailySchedule day="Пятница" pairs={fridayPairs} />
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
 
 export default TeachersSchedule;
