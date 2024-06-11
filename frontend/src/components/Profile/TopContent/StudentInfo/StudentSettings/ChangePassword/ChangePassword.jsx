05d4327891157a2ef06bfa9f19ecc5e9f782db1e
UFxx <smileprost@gmail.com>
2024-06-11 12:37:55 +0300

    Change password validation

--- frontend/src/components/Profile/TopContent/StudentInfo/StudentSettings/ChangePassword/ChangePassword.jsx
+++ frontend/src/components/Profile/TopContent/StudentInfo/StudentSettings/ChangePassword/ChangePassword.jsx
@@ -1,4 +1,24 @@
+import { useState } from "react";
+
 function ChangePassword(props) {
+  const [newPassword, setNewPassword] = useState();
+  const [repeatNewPassword, setRepeatNewPassword] = useState();
+  const [passwordIsValid, setPasswordIsValid] = useState();
+
+  function validateNewPassword(e) {
+    setRepeatNewPassword(e.target.value);
+    if (e.target.value === newPassword && e.target.value !== null) {
+      setPasswordIsValid(true);
+    } else {
+      setPasswordIsValid(false);
+    }
+  }
+
+  function changePassword() {
+    setNewPassword("");
+    setRepeatNewPassword("");
+  }
+
   return (
     <form
       className={`change-container__${
@@ -6,13 +26,29 @@ function ChangePassword(props) {
       }`}
     >
       <i className="fas fa-arrow-left" onClick={props.openChangePassword}></i>
-      <input type="password" placeholder="Введите новый пароль" />
       <input
         type="password"
-        id="new-password"
-        placeholder="Повторите новый пароль"
+        placeholder="Введите новый пароль"
+        value={newPassword}
+        onInput={(e) => setNewPassword(e.target.value)}
       />
-      <button>Применить</button>
+      <input
+        className={`new-password__${passwordIsValid ? "valid" : "invalid"}`}
+        type="password"
+        id="new-password"
+        value={repeatNewPassword}
+        placeholder="Повторите новый пароль"
+        onInput={validateNewPassword}
+      />
+      <button
+        onClick={changePassword}
+        type="button"
+        className={`password-submit-button button__${
+          passwordIsValid ? "visible" : "hidden"
+        }`}
+      >
+        Применить
+      </button>
     </form>
   );
 }
